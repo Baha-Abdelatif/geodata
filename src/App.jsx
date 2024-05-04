c4f3a27ce749d8dd517036e4415c83bf34b8baac
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-05 00:40:20 +0200

    Fetching countries datas from Rest API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-function App() {
-  return (
-    <div>
+import { useState } from "react";
+import { useEffect } from "react";
 
+function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const fetchDataForCountries = async () => {
+      try {
+        const response = await fetch(
+          "https://restcountries.com/v3.1/region/europe"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        const fetchedCountries = await response.json();
+        setCountries(fetchedCountries);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    fetchDataForCountries();
+  }, []);
+  return (
+    <div className='min-h-screen bg-slate-800'>
+      <div className='max-w-7xl mx-auto py-20 px-4'>
+        <h1 className='text-gray-50 text-4xl'>Europe countries</h1>
+        <p className='text-gray-100 text-xl mb-8'>
+          Select a card to reveal country's informations.
+        </p>
+      </div>
+      {countries.length > 0 &&
+        countries.map((country) => {
+          return (
+            <div key={country.cca2}>
+              <h3>{country.name.official}</h3>
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
