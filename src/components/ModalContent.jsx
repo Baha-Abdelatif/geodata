4ec710d80111e403caa4b1f7ded8a5469e4db652
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-05 16:48:41 +0200

    Create a Modal box to display informations about selected country

--- src/components/ModalContent.jsx
+++ src/components/ModalContent.jsx
@@ -8,6 +8,8 @@ const stylesClassName = {
   closeButton:
     "absolute top-2 right-2 w-8 h-8 flex items-center justify-center text-sm bg-red-600 text-white hover:bg-red-700 py-1 px-3 rounded",
   boxTitle: "text-2xl mb-4",
+  boxItem: "text-lg mb-2",
+  boxItemTitle: "font-semibold",
 };
 
 export default function ModalContent({ country, closeModal }) {
@@ -23,12 +25,29 @@ export default function ModalContent({ country, closeModal }) {
         <h2 className={stylesClassName.boxTitle}>
           Informations about {country.name.common}
         </h2>
-        <h3>Official Name: {country.name.official}</h3>
-        <p>Capital: {country.capital}</p>
-        <p>Language: {Object.values(country.languages).join(", ")}</p>
-        <p>Area: {country.area}Km²</p>
-        <p>Residents: {country.demonyms.eng.f}</p>
-        <p>Population: {country.population}</p>
+        <h3 className={stylesClassName.boxTitle}>
+          Official Name: {country.name.official}
+        </h3>
+        <p className={stylesClassName.boxItem}>
+          <span className={stylesClassName.boxItemTitle}>Capital:</span>{" "}
+          {country.capital}
+        </p>
+        <p className={stylesClassName.boxItem}>
+          <span className={stylesClassName.boxItemTitle}>Language(s):</span>{" "}
+          {Object.values(country.languages).join(", ")}
+        </p>
+        <p className={stylesClassName.boxItem}>
+          <span className={stylesClassName.boxItemTitle}>Area:</span>{" "}
+          {country.area}Km²
+        </p>
+        <p className={stylesClassName.boxItem}>
+          <span className={stylesClassName.boxItemTitle}>Residents:</span>{" "}
+          {country.demonyms.eng.f}
+        </p>
+        <p className={stylesClassName.boxItem}>
+          <span className={stylesClassName.boxItemTitle}>Population:</span>{" "}
+          {country.population} res.
+        </p>
       </div>
     </div>
   );
